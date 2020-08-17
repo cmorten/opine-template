69612ad9a987c5b2cf17792c7410667bcd928b78
asos-craigmorten <craig.morten@asos.com>
2020-08-17 15:52:26 +0100

    initial commit

---
+++ routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from "../deps.ts";
+
+const router = Router();
+
+// GET home page.
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Opine" });
+});
+
+export default router;
